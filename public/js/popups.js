b7c8a1e387c4b2788c2e4752a5955717a17b3e6c
Noah Scholz <noah.scholz.ns@gmail.com>
2022-05-04 00:24:54 +0200

    fixed scroll bug on post loading

--- public/js/popups.js
+++ public/js/popups.js
@@ -4,14 +4,6 @@ jQuery(function () {
         $("#newPostForm").css("visibility", "visible");
     })
 
-    // $("#createPostForm").on("submit", function (e) {
-    //     e.preventDefault();
-    //     this.submit();
-    //     $("#overlay").css("visibility", "hidden");
-    //     $("#newPostForm").css("visibility", "hidden");
-    //     location.reload();
-    // })
-
     $("#chancelNewPost").on("click", function () {
         $("#overlay").css("visibility", "hidden");
         $("#newPostForm").css("visibility", "hidden");
--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -90,7 +90,7 @@
 
                         loadMore(PAGE);
                         $(window).scroll(function() {
-                            if ($(window).scrollTop() + $(window).height() >= $(document).height()) {
+                            if ($(window).scrollTop() + $(window).height() >= $(document).height()-50) {
                                 PAGE++;
                                 loadMore(PAGE);
                             }
